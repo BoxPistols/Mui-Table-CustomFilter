e36cee07c14ca22ed58828dd6acabb0c79a94e08
ait <ait@ex.in>
2023-07-16 19:47:17 +0900

    refact re:set

--- src/components/Table/ApiFilterTable.tsx
+++ src/components/Table/ApiFilterTable.tsx
@@ -1,6 +1,5 @@
 // src/components/Table/ApiFilterTable.tsx
 import * as React from 'react'
-import { useState, useEffect } from 'react'
 import {
   Table,
   TableBody,
@@ -8,20 +7,21 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Box,
+  Pagination,
   Typography,
 } from '@mui/material'
+import { useState, useEffect } from 'react'
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward'
+
 import { StyledTableCell } from './StyledTableCell'
 import { StyledTableRow } from './StyledTableRow'
 import { SearchInput } from './SearchInput'
-import { useSort } from './useSort'
 import { ActionCell } from './ActionCell'
-import { Pagination } from '@mui/material'
-import { Box } from '@mui/system'
-import { useFilter } from './useFilter'
-import { SortIcon } from './SortIcon'
 
 // API data type
-export type Product = {
+type Product = {
   id: number
   title: string
   description: string
@@ -35,94 +35,202 @@ export type Product = {
   images: string[]
 }
 
+// Search in all fields of the object
+const multiFieldSearch = (row: Product, query: string) => {
+  // Define the fields to include in the search
+  const searchableFields: (keyof Product)[] = [
+    'title',
+    'description',
+    'price',
+    'rating',
+    'stock',
+    'brand',
+    'category',
+  ]
+
+  return searchableFields.some((field) =>
+    String(row[field]).toLowerCase().includes(query.toLowerCase()),
+  )
+}
+
 export const ApiFilterTable = () => {
   const [search, setSearch] = useState('')
   const isSearchEmpty = search === ''
+  const [filteredAndSortedRows, setFilteredAndSortedRows] = useState<Product[]>(
+    [],
+  )
+
   const [rows, setRows] = useState<Product[]>([]) // Defined rows as Product array
 
-  const filteredRows = useFilter(rows, search, ['title']) // 新たにフィルタリングを行うフックを追加
-
-  const { handleSort, sortField, sortDirection } = useSort(filteredRows)
+  const [sortField, setSortField] = useState<keyof Product | null>(null) // Defined sortField as keyof Product
+  const [sortDirection, setSortDirection] = useState('asc')
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://dummyjson.com/products')
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`)
-        }
-        const data = await response.json()
-        setRows(data.products)
-      } catch (error) {
-        console.error('An error occurred while fetching the data.', error)
-      }
-    }
-    fetchData()
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => setRows(data.products))
   }, [])
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  }
+
+  const handleClearSearch = () => {
+    setSearch('')
+    setPage(1)
+  }
+
+  const handleSort = (field: keyof Product) => {
+    let direction = 'asc'
+    if (sortField === field && sortDirection === 'asc') {
+      direction = 'desc'
+    }
+    setSortField(field)
+    setSortDirection(direction)
+  }
+
+  const SortIcon =
+    sortDirection === 'asc' ? (
+      <ArrowUpwardIcon
+        sx={{
+          fontSize: 20,
+          position: 'absolute',
+          top: '0.75em',
+          padding: '0 2px',
+        }}
+      />
+    ) : (
+      <ArrowDownwardIcon
+        sx={{
+          fontSize: 20,
+          position: 'absolute',
+          top: '0.75em',
+          padding: '0 2px',
+        }}
+      />
+    )
 
   // Pagination states
   const [page, setPage] = useState(1)
+
   const [itemsPerPage] = useState(10)
+
   const handlePageChange = (
     _event: React.ChangeEvent<unknown>,
     value: number,
   ) => {
     setPage(value)
   }
-  const pageCount = Math.ceil(filteredRows.length / itemsPerPage) // pageCountとpaginatedRowsはfilteredRowsを使用
-  const paginatedRows = filteredRows.slice(
+
+  const pageCount = Math.ceil(filteredAndSortedRows.length / itemsPerPage)
+  const paginatedRows = filteredAndSortedRows.slice(
     (page - 1) * itemsPerPage,
     page * itemsPerPage,
   )
+
+  useEffect(() => {
+    setFilteredAndSortedRows(
+      rows
+        .filter((row) => multiFieldSearch(row, search))
+        .sort((a, b) => {
+          if (sortField === null) return 0
+          if (a[sortField] < b[sortField]) {
+            return sortDirection === 'asc' ? -1 : 1
+          }
+          if (a[sortField] > b[sortField]) {
+            return sortDirection === 'asc' ? 1 : -1
+          }
+          return 0
+        }),
+    )
+  }, [rows, search, sortField, sortDirection, page]) // page added
 
   return (
     <>
       <SearchInput
         search={search}
-        handleSearchChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setSearch(e.target.value)
-        }
-        handleClearSearch={() => setSearch('')}
+        handleSearchChange={handleSearchChange}
+        handleClearSearch={handleClearSearch}
         isSearchEmpty={isSearchEmpty}
       />
 
       <TableContainer component={Paper}>
-        <Table aria-label="customized table">
+        <Table
+          sx={{ minWidth: 700, minHeight: 300 }}
+          aria-label="customized table"
+        >
           <TableHead>
             <TableRow>
               <StyledTableCell onClick={() => handleSort('title')}>
                 Title
-                {sortField === 'title' && (
-                  <SortIcon direction={sortDirection} />
-                )}
+                {sortField === 'title' && SortIcon}
               </StyledTableCell>
-              <StyledTableCell>Action</StyledTableCell>
+              <StyledTableCell onClick={() => handleSort('price')}>
+                Price
+                {sortField === 'price' && SortIcon}
+              </StyledTableCell>
+              <StyledTableCell onClick={() => handleSort('stock')}>
+                Stock
+                {sortField === 'stock' && SortIcon}
+              </StyledTableCell>
+              <StyledTableCell onClick={() => handleSort('rating')}>
+                rating
+                {sortField === 'rating' && SortIcon}
+              </StyledTableCell>
+              <StyledTableCell align="center" width={140}>
+                Action
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {paginatedRows.length > 0 ? (
               paginatedRows.map((row) => (
                 <StyledTableRow key={row.id}>
-                  <StyledTableCell>{row.title}</StyledTableCell>
+                  <StyledTableCell component="th" scope="row">
+                    {row.title}
+                  </StyledTableCell>
                   <StyledTableCell>{row.price}</StyledTableCell>
                   <StyledTableCell>{row.stock}</StyledTableCell>
                   <StyledTableCell>{row.rating}</StyledTableCell>
-                  <StyledTableCell>
+                  <StyledTableCell
+                    align="right"
+                    width={140}
+                    sx={{ whiteSpace: 'nowrap' }}
+                  >
                     <ActionCell row={row} />
                   </StyledTableCell>
                 </StyledTableRow>
               ))
             ) : (
               <StyledTableRow>
-                <StyledTableCell colSpan={5}>
+                <StyledTableCell
+                  component="th"
+                  scope="row"
+                  colSpan={5}
+                  sx={{ textAlign: 'center' }}
+                >
                   <Typography variant="h5">No results found.</Typography>
                 </StyledTableCell>
               </StyledTableRow>
             )}
-          </TableBody>
+          </TableBody>{' '}
         </Table>
       </TableContainer>
-      <Box display="flex" justifyContent="flex-end">
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginTop: 1,
+        }}
+      >
+        <Typography>
+          <Typography component="span" variant="caption">
+            {/* Total records: {sortedRows.length} /  */}
+            Total records: {filteredAndSortedRows.length} / Current page: {page}{' '}
+            / Total pages: {pageCount}
+          </Typography>
+        </Typography>
         <Pagination count={pageCount} page={page} onChange={handlePageChange} />
       </Box>
     </>
